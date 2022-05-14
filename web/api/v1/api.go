bc7a9ecd7c99d43f8b8054b7049fe8e2398680d9
Drew Stinnett <drew@drewlink.com>
2022-05-13 20:39:12 -0400

    Restructuring a little

--- web/api/v1/api.go
+++ web/api/v1/api.go
@@ -15,27 +15,3 @@ func GetFilm(c *gin.Context) {
 		"message": "pong",
 	})
 }
-
-func GetList(c *gin.Context) {
-	user := c.Param("user")
-	slug := c.Param("slug")
-	sc := c.MustGet("client").(letterboxd.ScrapeClient)
-	films, err := sc.List.ListFilms(nil, &letterboxd.ListFilmsOpt{
-		User:     user,
-		Slug:     slug,
-		LastPage: -1,
-	})
-	if err != nil {
-		c.JSON(500, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
-	//var data []*letterboxd.Film
-	//for _, film := range films {
-	//data = append(data, film)
-	//}
-	c.IndentedJSON(200, APIResponse{
-		Data: films,
-	})
-}
